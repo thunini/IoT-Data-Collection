b1781deaab069ff2822ff287512e274d31672ffb
Chanhee Lee <Chanhee@sinjisuui-iMac.local>
2023-10-03 14:26:28 +0900

    fixing conflicts

--- Aqara/get_stat_data.py
+++ Aqara/get_stat_data.py
@@ -8,10 +8,7 @@ import pandas as pd
 import datetime
 import calendar
 import logging
-<<<<<<< HEAD:Aqara/get_stat_data.py
 import subprocess
-=======
->>>>>>> 9296cf5c61ee6288374b58bc05a8386c5613520a:Aqara/test.py
 
     
 # reading input files
@@ -26,10 +23,6 @@ def read_input_files():
             inputs.append([line[0], line[1]])
             uid.append(line[2])
     return inputs, uid
-<<<<<<< HEAD:Aqara/get_stat_data.py
-=======
-
->>>>>>> 9296cf5c61ee6288374b58bc05a8386c5613520a:Aqara/test.py
 # generate signature
 def gen_sign(
     access_token: str,
@@ -91,17 +84,9 @@ def get_device_data():
             device_data[i]['updateTime'] =  datetime.datetime.fromtimestamp(device_data[i]['updateTime'] / 1000)
             df = pd.DataFrame(device_data[i], index=[i])
             device_df = pd.concat([device_df, df])
-<<<<<<< HEAD:Aqara/get_stat_data.py
         # today = datetime.date(2023, 5, 9)
         today = datetime.date.today()
         logger.debug(f"Device data: {device_df}")
-=======
-
-        # today = datetime.date(2023, 5, 9)
-        today = datetime.date.today()
-        logger.debug(f"Device data: {device_df}")
-
->>>>>>> 9296cf5c61ee6288374b58bc05a8386c5613520a:Aqara/test.py
         try:
             if os.path.isdir(f'./device_data/{today}/'):
                 device_df.to_csv(f'./device_data/{today}/{uid_lst[cnt]}_devices.csv')
@@ -117,7 +102,6 @@ def get_device_data():
         names = device_df['deviceName'].values.tolist()
         lst3 = []
         for i in range(len(lst)):
-<<<<<<< HEAD:Aqara/get_stat_data.py
             if lst2[i].strip() != "lumi.gateway.aqcn03" and lst2[i] != "lumi.sensor_ht.agl02":
                 lst3.append([lst[i], lst2[i], names[i]])
         sensor_lst.append(lst3)
@@ -137,19 +121,12 @@ def get_device_data_from_excel():
             # if lst2[i].strip() != "lumi.gateway.aqcn03" and lst2[i] != "lumi.sensor_ht.agl02":
                 lst3.append([lst[i], lst2[i], names[i]])
         sensor_lst.append(lst3)
-=======
-            if lst2[i] != "lumi.gateway.aqcn03" and lst2[i] != "lumi.sensor_ht.agl02":
-                lst3.append([lst[i], lst2[i], names[i]])
-        sensor_lst.append(lst3)
-        time.sleep(5)
->>>>>>> 9296cf5c61ee6288374b58bc05a8386c5613520a:Aqara/test.py
 
 # get sensor data
 def get_sensor_data():
     global sensor_lst
     for cnt in range(len(token_lst)):
         access_token = token_lst[cnt][0]
-<<<<<<< HEAD:Aqara/get_stat_data.py
         refresh_token = token_lst[cnt][1]
         today = datetime.date(2023, 5, 12)
         for i in range(28):
@@ -215,96 +192,12 @@ def get_sensor_data():
             # today = next_date + datetime.timedelta(1)
         # go to next UID
         cnt += 1
-=======
-        refresh_token = token_lst[cnt][1]    
-        today = datetime.date(2023, 5, 12)
-        for i in range(11):
-            next_date = today +  datetime.timedelta(3)
-            print(today)
-            print(next_date)
-
-            for sensors in sensor_lst:
-                for sensor in sensors:
-                    print(sensor)
-                    # yesterday = today - datetime.timedelta(days=3)
-                    timestamp = str(int(round(time.time() * 1000)))
-                    nonce = get_random_string(16)
-                    sign = gen_sign(access_token, app_id, key_id, nonce, timestamp, app_key)
-                    data = []
-                    json_data = { "intent": "fetch.resource.statistics",
-                        "data": {
-                            "resources": {
-                                "subjectId": f"{sensor[0]}",
-                                "resourceIds": get_resource_id(sensor[1])
-                            },
-                            "startTime": f"{calendar.timegm(today.timetuple()) * 1000 -  32400000}",
-                            "endTime": f"{calendar.timegm(next_date.timetuple()) * 1000 -  32400000}",
-                            "dimension": "30m",
-                            "size": 300
-                        }
-                    }
-                    with open("sensor_data.json", "w") as json_file:
-                        json.dump(json_data, json_file)
-
-                    try:
-                        curl = f""" curl -H "Content-Type":"application/json" -H "Accesstoken:{access_token}" -H "Appid:{app_id}" -H "Keyid:{key_id}" -H "Nonce:{nonce}" -H "Time:{timestamp}" -H "sign:{sign}" --data @sensor_data.json https://open-cn.aqara.com/v3.0/open/api """
-                        print("------------------------ Getting Sensor Data ---------------------------")
-                        response = os.popen(curl).read()
-                        data = json.loads(response)['result']['data']
-                    except Exception as e:
-                        print("error")
-                        logger.error(f"Error in getting sensor: {e}")
-
-                    df = pd.DataFrame()
-                    result = pd.DataFrame()
-
-                    if data != None:
-                        for i in reversed(range(len(data))):
-                            if data[i]['timeStamp'] != None:
-                                data[i]['timeStamp'] =  datetime.datetime.fromtimestamp(data[i]['timeStamp'] / 1000)
-                                data[i]['timeStamp'] =  data[i]['timeStamp'].strftime("%m/%d/%Y, %H:%M:%S")
-
-                            data[i]['startTimeZone'] =  datetime.datetime.fromtimestamp(data[i]['startTimeZone'] / 1000)
-                            data[i]['startTimeZone'] =  data[i]['startTimeZone'].strftime("%m/%d/%Y, %H:%M:%S")
-                            data[i]['endTimeZone'] =  datetime.datetime.fromtimestamp(data[i]['endTimeZone'] / 1000)
-                            data[i]['endTimeZone'] =  data[i]['endTimeZone'].strftime("%m/%d/%Y, %H:%M:%S")
-                            df = pd.DataFrame(data[i], index=[i])
-                            result = pd.concat([result, df])
-
-                        try:
-                            if os.path.isdir(f'./sensor_data/{uid_lst[cnt]}/{today}'):
-                                filename = f'./sensor_data/{uid_lst[cnt]}/{today}/{sensor[2]}_{sensor[0]}.csv'
-                                result.to_csv(filename)
-                                print(f"file saved {filename}")
-                            else:
-                                os.makedirs(f'./sensor_data/{uid_lst[cnt]}/{today}')
-                                filename = f'./sensor_data/{uid_lst[cnt]}/{today}/{sensor[2]}_{sensor[0]}.csv'
-                                result.to_csv(filename)
-                                print(f"file saved {filename}")
-                        except Exception as e:
-                            print("Error in saving sensor data")
-                            logger.error(f"Error in saving sensor data: {e}")
-                    time.sleep(6)
-            
-            # go to next date
-            today = next_date
-            # today = next_date + datetime.timedelta(1)
-
-
-        # go to next UID
-        cnt += 1
-        
->>>>>>> 9296cf5c61ee6288374b58bc05a8386c5613520a:Aqara/test.py
 # main function
 def main():
     global token_lst, app_id, app_key, key_id, sensor_lst, logger, uid_lst
     timestamp = datetime.datetime.now()
     logging.basicConfig(
-<<<<<<< HEAD:Aqara/get_stat_data.py
         # filename=f'logs/aqara.log_{timestamp}',
-=======
-        filename=f'logs/aqara.log_{timestamp}',
->>>>>>> 9296cf5c61ee6288374b58bc05a8386c5613520a:Aqara/test.py
         format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
         datefmt='%Y-%m-%d:%H:%M:%S',
         level=logging.DEBUG)
@@ -314,17 +207,12 @@ def main():
     app_id = "1082333016935813120e603a"
     app_key = "9b4otxco3vq06z0c6fqfb30da1mt05l1"
     key_id = "K.1082333016981950466"
-<<<<<<< HEAD:Aqara/get_stat_data.py
-=======
-
->>>>>>> 9296cf5c61ee6288374b58bc05a8386c5613520a:Aqara/test.py
     token_lst, uid_lst = read_input_files()
     # Access Mongodb
     # conn = pymongo.MongoClient("mongodb://pymongo:pymongo@server1.iclab.dev:3001/")
     # db = conn.get_database("testDB")
     # sensor_data_coll = db.get_collection("aqara_sensor_data")
     # device_data_coll = db.get_collection("aqara_device_data")
-<<<<<<< HEAD:Aqara/get_stat_data.py
     while True:
         sensor_lst = []
         # get_device_data()
@@ -332,18 +220,5 @@ def main():
         time.sleep(1)
         get_sensor_data()
         break
-=======
-
-    while True:
-        sensor_lst = []
-        get_device_data()
-        time.sleep(5)
-        for i in range(len(sensor_lst)):
-            sensor_lst[i].pop(0)
-        get_sensor_data()
-        break
-
-
->>>>>>> 9296cf5c61ee6288374b58bc05a8386c5613520a:Aqara/test.py
 if __name__ == "__main__":
     main()
