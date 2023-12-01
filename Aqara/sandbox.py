6b95c4f9582abba48bf6c1632626ba8a4fb19882
Chanhee Lee <Chanhee@sinjisuui-iMac.local>
2023-12-01 13:42:11 +0900

    backup

--- Aqara/sandbox.py
+++ Aqara/sandbox.py
@@ -1,16 +1,27 @@
-import datetime
-import calendar
-import time
+from aqara_msg_push import MessageConsumer, TopLevelMessage
+from rocketmq.client import ReceivedMessage, ConsumeStatus
 
-today = datetime.date(2023, 9, 1)  # Include time information
+# Your params from https://developer.aqara.com/console/overview
+ADDRESS = 'some-broker1.aqara.com:9876'
+TOPIC = 'sensor monitoring' 
+GROUP_ID = '99..ce'
+ACCESS_KEY = 'K....'
+SECRET_KEY = 'nt..jg'
 
-for i in range(9):
-    next_date = today +  datetime.timedelta(3)
-    for j in range(2):
-        today = today +  datetime.timedelta(1)
+KEY_ID = ACCESS_KEY
 
-        print(today)
-        print(next_date)
-    today = next_date
 
-    
+class MyMessageConsumer(MessageConsumer):
+
+    def callback(self, obj: TopLevelMessage, raw_msg: ReceivedMessage) -> ConsumeStatus:
+        print(type(obj), obj.data)
+        return ConsumeStatus.CONSUME_SUCCESS
+
+
+MyMessageConsumer(
+    group_id=GROUP_ID,
+    access_key=ACCESS_KEY,
+    secret_key=SECRET_KEY,
+    topic=TOPIC,
+    address=ADDRESS,
+).consume()
