7937dfa41b1bda7a6817ab0dd86201343f6d8c83
Chanhee Lee <Chanhee@sinjisuui-iMac.local>
2023-12-07 12:23:02 +0900

    backup

--- Aqara/get_data_history.py
+++ Aqara/get_data_history.py
@@ -9,6 +9,7 @@ import datetime
 import calendar
 import logging
 import subprocess
+import requests
 
 # reading input files
 def read_input_files():
@@ -23,6 +24,13 @@ def read_input_files():
             uid.append(line[2])
     return inputs, uid
 
+# write newly retrieved tokens to input.txt
+def write_tokens_to_file(access_token, refresh_token, user, cnt):
+    global strings
+    strings[cnt] = f'{access_token} {refresh_token} {user}\n'
+    with open("input.txt", 'w') as file:
+        file.writelines(strings)
+
 # generate signature
 def gen_sign(
     access_token: str,
@@ -32,7 +40,6 @@ def gen_sign(
     timestamp: str,
     app_key: str,
 ):
-    """Signature in headers, see https://opendoc.aqara.cn/docs/%E4%BA%91%E5%AF%B9%E6%8E%A5%E5%BC%80%E5%8F%91%E6%89%8B%E5%86%8C/API%E4%BD%BF%E7%94%A8%E6%8C%87%E5%8D%97/Sign%E7%94%9F%E6%88%90%E8%A7%84%E5%88%99.html"""
     s = f"Appid={app_id}&Keyid={key_id}&Nonce={nonce}&Time={timestamp}{app_key}"
     if access_token and len(access_token) > 0:
         s = f"AccessToken={access_token}&{s}"
@@ -61,9 +68,35 @@ def get_resource_id(model):
         return ["13.1.85"]
 
 # refresh access token
-def refresh_access_token():
-    print('a')
+def refresh_access_token(access_token, refresh_token):
+    global token_lst, app_id, app_key, key_id
 
+    timestamp = str(int(round(time.time() * 1000)))
+    nonce = get_random_string(16)
+    sign = gen_sign(access_token, app_id, key_id, nonce, timestamp, app_key)
+
+    refresh_json = {
+        "intent": "config.auth.refreshToken",
+        "data": {
+            "refreshToken": f"{refresh_token}"
+        }
+    }
+
+    with open("refresh_data.json", "w") as json_file:
+        json.dump(refresh_json, json_file)
+
+    try:
+        curl = f""" curl -H "Content-Type":"application/json" -H "Appid:{app_id}" -H "Keyid:{key_id}" -H "Nonce:{nonce}" -H "Time:{timestamp}" -H "sign:{sign}" --data @refresh_data.json https://open-cn.aqara.com/v3.0/open/api """
+        response = subprocess.check_output(curl, shell=True, encoding='utf-8')
+        response_data = json.loads(response)
+        print(response_data)
+        new_access_token = response_data['result']['accessToken']
+        new_refresh_token = response_data['result']['refreshToken']
+        time.sleep(2)
+        return [new_access_token, new_refresh_token]
+    except Exception as e:
+        logger.error(f"Error in getting sensor data: {e}")
+        return []
 
 
 # get device data
@@ -71,7 +104,6 @@ def get_device_data():
     global token_lst, app_id, app_key, key_id, sensor_lst
     for cnt in range(len(token_lst)):
         access_token = token_lst[cnt][0]
-        refresh_token = token_lst[cnt][1]
         timestamp = str(int(round(time.time() * 1000)))
         nonce = get_random_string(16)
         sign = gen_sign(access_token, app_id, key_id, nonce, timestamp, app_key)
@@ -79,6 +111,7 @@ def get_device_data():
         device_data = []
         device_df = pd.DataFrame()
         curl = f""" curl -H "Content-Type":"application/json" -H "Accesstoken:{access_token}" -H "Appid:{app_id}" -H "Keyid:{key_id}" -H "Nonce:{nonce}" -H "Time:{timestamp}" -H "sign:{sign}" --data @device_data.json https://open-cn.aqara.com/v3.0/open/api"""
+
         try:
             response = subprocess.check_output(curl, shell=True, encoding='utf-8')
             response_data = json.loads(response)
@@ -130,7 +163,7 @@ def get_device_data_from_excel():
                 lst3.append([lst[i], lst2[i], names[i]])
         sensor_lst.append(lst3)
 
-# 
+# get data history for data types that exceed size 300 by iteration
 def get_data_history_iter(access_token, sensor, cnt, today, today_date, iter, hour):
     next_date = today + datetime.timedelta(hours=hour)
     for count in range(iter):
@@ -189,16 +222,16 @@ def get_data_history_iter(access_token, sensor, cnt, today, today_date, iter, ho
         time.sleep(4)
 
 # get sensor data
-def get_sensor_data():
+def get_sensor_data(year, month, day):
     global sensor_lst
     for cnt in range(len(token_lst)):
         access_token = token_lst[cnt][0]
         refresh_token = token_lst[cnt][1]
-        today_date = datetime.date(2023, 10, 17) - datetime.timedelta(2)
+        today_date = datetime.date(year, month, day) - datetime.timedelta(2)
         for sensor in sensor_lst[cnt]:
 
-            today = datetime.datetime(2023, 10, 17, 0, 0) - datetime.timedelta(2)
-            next_date = datetime.datetime(2023, 10, 17, 0, 0)
+            today = datetime.datetime(year, month, day, 0, 0) - datetime.timedelta(2)
+            next_date = datetime.datetime(year, month, day, 0, 0)
             
             # cut endtime - start time to 12 hours for env and 4 hours for smart plug
             if sensor[1] == "lumi.sen_ill.agl01":
@@ -272,18 +305,27 @@ def main():
     app_key = "9b4otxco3vq06z0c6fqfb30da1mt05l1"
     key_id = "K.1082333016981950466"
     token_lst, uid_lst = read_input_files()
+    year = int(input("Enter year: "))
+    month = int(input("Enter month: "))
+    day = int(input("Enter day: "))
+
+    # for user in range(len(token_lst)):
+    #     [new_access_token, new_refresh_token] = refresh_access_token(token_lst[user][0], token_lst[user][1])
+    #     token_lst.append([new_access_token, new_refresh_token])
+    #     write_tokens_to_file(new_access_token, new_refresh_token, uid_lst[user], user)
+
+    
 
     while True:
         sensor_lst = []
         # get_device_data()
         get_device_data_from_excel()
         time.sleep(1)
-        get_sensor_data()
+        get_sensor_data(year, month, day)
         # send Slack notification
         # curl = """ curl -X POST -H 'Content-type: application/json' --data '{"text":"Finished collecting Aqara sensor data"}' https://hooks.slack.com/services/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 """
         # subprocess.check_output(curl, shell=True, encoding='utf-8')
         break
-
 
 if __name__ == "__main__":
     main()
